Added 'place_sensor', look before you move, and fixed PP stopping lookahead away

low_level_controller/Face_Forward.py
@@ -5,6 +5,7 @@ from scipy.spatial.transform import Rotation as R
 
 class FaceForward():
     def __init__(self):
+        self.facing_forward = False
         self.prev_angular_z = 0
         self.kp = 1
         self.kd = 0.2
@@ -43,8 +44,16 @@ class FaceForward():
         # in case we are not moving the drone
         if np.isnan(omega_z):
             omega_z = 0
-            
+
+        # let the drone face motion direction before starting to move
+        if not self.facing_forward:
+            vx, vy = 0, 0
+
+        if abs(rotation_angle) < 0.5:
+            self.facing_forward = True
+
+        # publish angle command for debugging purposes
         self.debug_pub.publish(omega_z)
         self.prev_angular_z = omega_z
-        #publish angle command
-        return (vx, vy, vz), omega_z
+        
+        return self.facing_forward, (vx, vy, vz), omega_z

low_level_controller/Pure_Pursuit.py
@@ -10,7 +10,7 @@ class PurePursuit():
     def __init__(self, path=None, odom=None, pose=None, threshold=0):
         self.fly_cmd = Twist()
         self.lookahead = 3
-        self.speed = 0.5
+        self.speed = 1.5
         self.path = path
         self.path_segments = None
         self.pose = pose
@@ -30,6 +30,10 @@ class PurePursuit():
         self.point_pub = rospy.Publisher("/pursuit_point", PointStamped, queue_size=1)
         self.pose_pub = rospy.Publisher("/pp_pose", Point32, queue_size=1)
         self.stop = False
+
+        #waypoint constraints
+        self.waypoint_threshold = 0.3 # how close we need to be to the waypoint
+        self.pursuit_epsilon = 0.1 #allows us to satisfy |x_f - x_d| < waypoint_threshold
 
     # Compute Euclidean distance between 2 points
     def distance(self, point1, point2):
@@ -101,67 +105,70 @@ class PurePursuit():
         min_distances = np.apply_along_axis(self.minimum_distance(point), 1, self.path_segments)
         min_index = np.argmin(min_distances)
 
-        forward_path = self.path_segments[min_index:][::-1]
-        desired_point = self.path_segments[min_index][:2] # default desired point as endpoint of closest line segment of trajectory
-        # print(desired_point)
+        # no need to look ahead of only one path segment left, just to to the end
+        desired_point = self.path_segments[min_index][2:]
 
-        #potentially repace with somethiing much simpler. Maybe just take the first segment
+        # modulate speed if getting close to the waypoint
+        self.speed = 1.5 if len(self.path_segments) > 5 else 0.5
 
-        for i, segment in enumerate(forward_path):
-            # start and end points of each segment 
-            start, end = np.array([segment[0], segment[1]]), np.array([segment[2],segment[3]])
+        if len(self.path_segments) != 1:
+            forward_path = self.path_segments[min_index:][::-1]
 
-            V = end - start  # Vector along line segment
+            for i, segment in enumerate(forward_path):
+                # start and end points of each segment 
+                start, end = np.array([segment[0], segment[1]]), np.array([segment[2],segment[3]])
 
-            a = np.dot(V,V)
-            b = 2 * np.dot(V, start - point)
-            c = np.dot(start, start) + np.dot(point, point) - 2 * np.dot(start, point) - self.lookahead**2
+                V = end - start  # Vector along line segment
 
-            # Discriminant
-            disc = b**2 - 4*a*c
+                a = np.dot(V,V)
+                b = 2 * np.dot(V, start - point)
+                c = np.dot(start, start) + np.dot(point, point) - 2 * np.dot(start, point) - self.lookahead**2
 
-            if disc < 0: # no real solutions
-                continue 
+                # Discriminant
+                disc = b**2 - 4*a*c
 
-            sqrt_disc = np.sqrt(disc)
-            t1 = (-b + sqrt_disc) / (2 * a)
-            t2 = (-b - sqrt_disc) / (2 * a)
+                if disc < 0: # no real solutions
+                    continue 
 
-            if not (0 <= t1 <= 1 or 0 <= t2 <= 1):
-                continue
+                sqrt_disc = np.sqrt(disc)
+                t1 = (-b + sqrt_disc) / (2 * a)
+                t2 = (-b - sqrt_disc) / (2 * a)
 
-            t = max(0, min(1, - b / (2 * a)))
-            desired_point = start + t * V
-            min_index += len(forward_path) - i - 1
-            break
+                if not (0 <= t1 <= 1 or 0 <= t2 <= 1):
+                    continue
+
+                t = max(0, min(1, - b / (2 * a)))
+                desired_point = start + t * V
+                min_index += len(forward_path) - i - 1
+                break
 
         #FOR DEBUGGING
         self.point_pub.publish(self.createPoint(desired_point))
-        # print(desired_point)
         path_vector = desired_point - point
         new_yaw = np.arctan2(path_vector[1],path_vector[0])
-        eta = new_yaw #- pose[-1] 
+        eta = new_yaw
 
         #UPDATE DRONE POSITION 
         # obtain x and y components of eta to command movement
         # hard code flight speed for now (1)
         v_x = self.speed*np.cos(eta)
         v_y = self.speed*np.sin(eta)
-        # print(v_x, v_y)
 
         self.path_segments = self.path_segments[min_index:]
 
         #CHANGE LOOKAHEAD BASED ON CORNERS
-        if (len(self.path_segments) == 1 and self.distance(point, self.path_segments[-1][2:]) < self.lookahead):
+        # if eta < (np.pi/6): self.lookahead = 3.0
+        # else: self.lookahead = 1.5
+
+        # If close enough to waypoint, end iteration:
+        if self.distance(point, self.path_segments[-1][2:]) < self.waypoint_threshold:
             self.stop = True
-            return True, np.array([0, 0, 0])
+            return True,np.array([0,0,0])
 
-        if eta < (np.pi/6): self.lookahead = 3.0
-        else: self.lookahead = 1.5
-
-        #if env is too close to drone
+        '''Ground laser PD Control'''
+        # if env is too close to drone
         if self.within_threshold:
-            print('violated threshold')
+            print("violated threshold")
             v_z = self.threshold-self.ground_dist
         # nominal threshold present, control global z value
         else:

motion_planners/Navigator.py
@@ -52,6 +52,7 @@ class Navigator():
         self.facing_forward = False
         # ROS relevant initializations
         self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.vel_pub_debug = rospy.Publisher('/cmd_vel_debug', Twist, queue_size=1)
         self.odom_sub = rospy.Subscriber('/ground_truth/state', Odometry, self.odom_callback)
         self.range_sub = rospy.Subscriber('/sonar_height', Range, self.range_callback)
         self.waypoint_sub = rospy.Subscriber('/waypoint_topic', Point, self.waypoint_callback)
@@ -76,10 +77,14 @@ class Navigator():
         """
         self.waypoint = np.array([msg.x, msg.y, self.flight_height])
         if not np.all(self.last_waypoint == self.waypoint):
+            print("New Waypoint received! Initializaing variables")
+            self.initialize_hover = True
             self.done_travelling.data = False
             self.path_plan = True
             self.path_found = True
+            self.facing_forward = False
             self.last_waypoint = self.waypoint
+            self.finished_pub.publish(self.done_travelling)
 
     def occupancy_callback(self, msg):
         """
@@ -131,6 +136,7 @@ class Navigator():
         # no occupancy grid provided yet
         if self.occupancy_grid is None:
             print("No occupancy grid provided")
+            return
 
         pose = msg.pose.pose
         # determine where the drone is currently located
@@ -166,6 +172,7 @@ class Navigator():
                 # planner failed to find a path to the next waypoint. Stay in place
                 if self.path is None:
                     print("Failed to find path. Staying in place")
+                    # raise Exception
                     self.path_found = False
                     # Let the drone just stay in place
                     self.fly_cmd.linear.x=0
@@ -206,29 +213,32 @@ class Navigator():
             self.done_travelling.data, v_world = self.llc.actuate()
             self.prev_v_world = v_world
 
+            ########################################
+            # self.facing_forward to make drone face 
+            # direction of movement before moving
+            ########################################
             # second low-level controller so the drone faces the direction of motion
             self.ffc.prev_angular_z = self.prev_angular_z
-            v_drone, omega_z = self.ffc.face_forward_control(v_world, pose)
-            #######################################################
-            # potentailly face motion direction first and then move
-            #######################################################
+            self.ffc.facing_forward = self.facing_forward
+            self.facing_forward, v_drone, omega_z = self.ffc.face_forward_control(v_world, pose)
 
             self.fly_cmd.linear.x, self.fly_cmd.linear.y, self.fly_cmd.linear.z = v_drone
             self.fly_cmd.angular.z = omega_z
             self.prev_angular_z = omega_z
-            self.vel_pub.publish(self.fly_cmd)
-
+            # self.vel_pub.publish(self.fly_cmd)
+            self.vel_pub_debug.publish(self.fly_cmd)
+            
         # all waypoints reached, no need to do anything anymore
         else:
             if not self.path_found:
                 print(f"We failed to find paths. We stopped at waypoint: {self.waypoint}")
-                return False
+                # return False
             else:
                 print("Reached Waypoint!")
                 self.done_travelling.data = True
-                self.finished_pub.publish(self.done_travelling.data)
-                rate.sleep()
-                return True
+        
+        # indicate whether we are done travelling or not
+        self.finished_pub.publish(self.done_travelling.data)
 
     def range_callback(self, msg) -> None:
         """

motion_planners/planners.py
@@ -3,14 +3,13 @@ import numpy as np
 from node import Node
 
 class Planners():
-    def __init__(self,  algo: str, world_dims: tuple, occupancy_grid: np.ndarray, flight_height: int, debug = False):
+    def __init__(self,  algo: str, world_dims: tuple, occupancy_grid: np.ndarray, flight_height: int):
         self.algos = ['rrt','a_star']
         self.algo = algo
         self.min_x, self.max_x, self.min_y, self.max_y = world_dims
         self.occupancy_grid = occupancy_grid
         self.max_row, self.max_col = np.array(self.occupancy_grid.shape) - 1
         self.flight_height = flight_height
-        self.debug = debug
 
     def plan(self, goal: np.ndarray, start: np.ndarray):
         path = None
@@ -18,14 +17,14 @@ class Planners():
             raise Exception('not a valid algo')
         
         if self.algo == 'rrt':
-            status, path = self.rrt(goal,start,self.debug)
+            status, path = self.rrt(goal,start)
         
         elif self.algo == 'a_star':
-            status, path = self.a_star(goal,start,self.debug)
+            status, path = self.a_star(goal,start)
 
         return status, path    
 
-    def a_star(self, goal: np.ndarray, start: np.ndarray, debug=False) -> tuple:
+    def a_star(self, goal: np.ndarray, start: np.ndarray) -> tuple:
         """
         This function will generate a path from location start to location goal. It will generate a plan
         that avoids obstacles by planning over the provided map in self.map
@@ -43,13 +42,9 @@ class Planners():
         """
         path_found = False
         path = None
-        # testing will pass in row, col grid locations directly
-        if debug:
-            start_grid = start
-            end_grid = goal
-        else:
-            start_grid = np.array(self.meters_to_grid(*start[:2]))
-            end_grid = np.array(self.meters_to_grid(*goal[:2]))
+
+        start_grid = np.array(self.meters_to_grid(*start[:2]))
+        end_grid = np.array(self.meters_to_grid(*goal[:2]))
 
         # note that that both nodes and the priority queue operate in u,v space, not x,y space
         start_node = Node(start_grid)
@@ -118,7 +113,7 @@ class Planners():
                 break
         points.reverse()
         return points
-    
+
     def get_neighbors(self, loc: np.ndarray) -> set:
         """
         Checks the neighbors of the current grid location we are at and returns the set that are free.
@@ -137,14 +132,14 @@ class Planners():
         for translation in translations:
             new_loc = loc + translation
             # check validity of new location (make sure we didn't go outside the map)
-            if np.any(new_loc < 0) or np.any(new_loc == np.array(self.occupancy_grid.shape)):
+            if np.any(new_loc < 0) or np.any(new_loc >= np.array(self.occupancy_grid.shape)):
                 continue
             # only add unoccupied locations and nodes unvisited by the A-star algorithm
             if self.occupancy_grid[new_loc[0], new_loc[1]] == 0:
                 free.add(tuple(new_loc))
         return free
     
-    def rrt_planner(self, goal: np.ndarray, start: np.ndarray) -> None:
+    def rrt(self, goal: np.ndarray, start: np.ndarray) -> None:
         """
         This function will generate a path from location start to location goal by using RRT. It will 
         generate a plan that avoids obstacles by planning over the provided map in self.map. Stores the
@@ -162,13 +157,17 @@ class Planners():
         path_found = False
         valid_set = {Node(start)}
         free_grid_points = np.argwhere(self.occupancy_grid == 0)
-        d = 5
+        d = 0.5
         tol = d*1.0
+        max_iterations = 1000
+        iteration = 0
+        path_found = False
+        path = None
 
         # Repeat RRT graph generation until goal reached
-        while not path_found:
+        while not path_found and iteration < max_iterations:
             # genereate random point in space, goal biased-parameter
-            if np.random.uniform() < 0.0:
+            if np.random.uniform() < 0.2:
                 pos_rand = goal
                 goal_sampled = True
             else:
@@ -201,7 +200,12 @@ class Planners():
             if np.linalg.norm(new_pos - goal) < tol:
                 path_found = True
                 print('path found')
-                self.extract_path_RRT(new_node)
+                path = self.extract_path_RRT(new_node)
+                path_found = True
+                break
+            
+            iteration += 1
+        return path_found, path
 
     def extract_path_RRT(self, final_node):
         node = final_node