dd44c7ebef63cc9c684d6c1df97649b3f71cab97
frankgon1627 <fg1333762@gmail.com>
2023-05-14 13:35:44 -0400

    made csv files

--- analysis/plots/make_rosbag_plot.py
+++ analysis/plots/make_rosbag_plot.py
@@ -3,14 +3,17 @@ import pandas as pd
 import numpy as np
 
 if __name__ == '__main__':
-    csv_path = '~/catkin_ws/src/Motion-Planning/analysis/rosbags/naive_A_to_B_Controller/sonar_height.csv'
+    error_path = '~/catkin_ws/src/Motion-Planning/analysis/rosbags/waypoint_navigation/first_waypoint_pp_error.csv'
+    max_error_path = '~/catkin_ws/src/Motion-Planning/analysis/rosbags/waypoint_navigation/first_waypoint_pp_max_error.csv'
 
-    columns = ["%time", "field.header.seq", "field.header.stamp", "field.header.frame_id", "field.radiation_type", "field.field_of_view", "field.min_range", "field.max_range", "field_range"]
-    df = pd.read_csv(csv_path, usecols=columns)
-    plt.title("Naive Controller Performance")
-    plt.plot(df.field_range, label="Sonar Reading [m]")
-    plt.plot(1 - df.field_range, label="Ground Height Error [m]")
-    plt.plot(np.arange(0, 487), np.ones(487), label="Target Height [m]")
+    columns = ["time", "field_data"]
+    df_path_error = pd.read_csv(error_path, usecols=columns)
+    time_initial = df_path_error.time[0]
+    time = (np.array(df_path_error.time) - time_initial)/1e9
+    path_deviation = df_path_error.field_data
+    plt.title("Pure Pursuit Performance")
+    plt.plot(time, path_deviation, label="Path Deviation [m]")
+    # plt.plot(time deviation, label="Maximum Deviation [m]")
     plt.legend()
-    plt.savefig('naive_A_to_B_performance')
-    plt.show()
+    plt.savefig('PP_waypoint1')
+    # plt.show()
